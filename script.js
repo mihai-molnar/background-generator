037dd0017459f80c7c18dcdf3ae726b34717dc12
Mihai <mihai.molnar1984@icloud.com>
2019-11-23 15:55:03 +0200

    addig the project files

---
+++ script.js
@@ -0,0 +1,23 @@
+//Define the variables
+var css = document.querySelector("h3"); //h3
+var color1 = document.querySelector(".color1"); //input color 1
+var color2 = document.querySelector(".color2"); //input color 2
+var body = document.getElementById('gradient');
+var h3 = document.getElementById("h3title")
+
+function changeColor() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+	h3.innerHTML = "First color: " + color1.value + " Second color: " + color2.value;
+}
+
+
+color1.addEventListener("input", changeColor)
+
+color2.addEventListener("input", changeColor)
+
+console.log(h3);
